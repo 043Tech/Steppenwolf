cb0822894ad9fa7e98e299c0beae02640a2fc71b
Vutov <VutovSpas@gmail.com>
2020-04-14 21:02:52 +0300

    [Add] Stylecop

--- src/Steppenwolf/Program.cs
+++ src/Steppenwolf/Program.cs
@@ -1,13 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Debugging;
 using Serilog.Events;
@@ -44,9 +37,6 @@ namespace Steppenwolf
                     
                     SelfLog.Enable(Console.Error);
                 })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
